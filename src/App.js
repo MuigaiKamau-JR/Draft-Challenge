ccfcfdcf86bc17ce01ee930f8bee208313465aad
Kamau <kamauwamuigaijr@gmail.com>
2024-05-09 12:24:25 +0300

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import BotCollection from './components/BotCollection';
+import BotArmy from './components/BotArmy';
+import SortBar from './components/SortBar';
+import './App.css'; // Import your CSS file for styling
 
-function App() {
+export default function App() {
+  const botsAPI = 'https://bots-taupe.vercel.app/bots';
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch(botsAPI)
+      .then((res) => res.json())
+      .then(setBots);
+  }, []);
+
+  function enlistBot(id) {
+    setBots(bots.map((bot) => (id === bot.id ? { ...bot, isEnlisted: true } : bot)));
+  }
+
+  function delistBot(id) {
+    setBots(bots.map((bot) => (id === bot.id ? { ...bot, isEnlisted: false } : bot)));
+  }
+
+  function dischargeBot(id) {
+    setBots(bots.filter((bot) => (bot.id === id ? false : true)));
+  }
+
+  // Function to sort bots by criteria (health, damage, or armor)
+  function handleSort(criteria) {
+    const sortedBots = [...bots].sort((a, b) => b[criteria] - a[criteria]);
+    setBots(sortedBots);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="app-heading">Bot Battlr</h1>
+      <div className="app-content">
+        <SortBar handleSort={handleSort} />
+        <BotArmy bots={bots.filter((bot) => bot.isEnlisted)} handleDischarge={dischargeBot} handleClick={delistBot} />
+        <BotCollection bots={bots} handleDischarge={dischargeBot} handleClick={enlistBot} />
+      </div>
     </div>
   );
 }
-
-export default App;
